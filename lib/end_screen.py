90275c5566122921783d701eb9ccf41a03493fcb
cody <w27101994@gmail.com>
2020-06-17 03:39:11 +0800

    fuck

--- lib/end_screen.py
+++ lib/end_screen.py
@@ -5,14 +5,16 @@ from lib import Screen
 class EndScreen(Screen):
     def __init__(self, screen):
         self.screen = screen
-        self.text = None
+        self.text1 = None
+        self.text2 = None
 
     def Draw(self, s):
         s = 1 if s == 1 else 2
         my_font = pygame.font.Font("./resources/font/Pixelony.ttf", 160)
-        text_surface = my_font.render("Player{s} \n Win!".format(s=s), True, (0,0,0), (255, 255, 255))
-        self.text = text_surface
-
+        text_surface1 = my_font.render("Player{s}".format(s=s), True, (0,0,0), (255, 255, 255))
+        text_surface2 = my_font.render("Win!", True, (0,0,0), (255, 255, 255))
+        self.text1 = text_surface1
+        self.text2 = text_surface2
     def Exec(self, s):
         pygame.mixer.init()
         pygame.mixer.music.load("./resources/BGM/start.mp3")
@@ -26,6 +28,7 @@ class EndScreen(Screen):
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill((0, 0, 0))
-            self.screen.blit(self.text, (50, 100))
+            self.screen.blit(self.text1, (40, 100))
+            self.screen.blit(self.text2, (250, 300))
             pygame.display.flip()
             clock.tick(50)
